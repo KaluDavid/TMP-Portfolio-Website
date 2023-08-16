c27b552237379f667a12b331de3c9854f9028a97
Dave <102557749+KaluDavid@users.noreply.github.com>
2023-08-16 03:48:45 +0100

    hamburger commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// hamburger menu
+function myFunction() {
+    var x = document.getElementById("visible");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
